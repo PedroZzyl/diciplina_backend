da3554428dc8ac0170829a6b3493d97753b463fb
Pedro Lucas Santos Lopes <11843443619@DC01.IFS.IFSULDEMINAS.EDU.BR>
2022-06-06 09:20:57 -0300

    correção da Conexão

--- projeto/banco/conexao.php
+++ projeto/banco/conexao.php
@@ -3,11 +3,11 @@
   $maquina = "127.0.0.1";
   $usuario = "root";
   $senha = "";
-  $banco = "db_3infob";
+  $banco = "mydb";
+  $porta = 3366;
 
   //cria uma conexão com o banco de dados no mysql
-  $conexao = new mysqli($maquina, 
-                        $usuario, $senha,  $banco);
+  $conexao = new mysqli($maquina, $usuario, $senha, $banco, $porta);
   
   //verifica se houve erros na conexão
   if($conexao->connect_error){
